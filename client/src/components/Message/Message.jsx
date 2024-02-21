733ddfd3b284415b413e47394818de0de26c38b3
gaurang <gaurang8102003@gmail.com>
2024-02-21 14:22:40 +0530

    message changes

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -1,13 +1,23 @@
+import { useSelector } from "react-redux";
 import "./message.css";
 import { format } from "timeago.js";
 
 export default function Message({ message, own }) {
+
+  const otherUserDetails = useSelector((state) => state.chat.otherUserDetails);
+  const user = useSelector((state) => state.auth.user);
+
   return (
     <div className={own ? "message own" : "message"}>
       <div className="messageTop">
         <img
           className="messageImg"
-          src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          src={
+            own 
+              ? ( user?.profilePicture || "https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" )
+              : ( otherUserDetails?.profilePicture || "https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" )
+          
+          }
           alt=""
         />
         <p className="messageText">{message?.text}</p>
